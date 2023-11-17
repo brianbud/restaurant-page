134b7d41e0eaf89b8ba2a6742b9042bc7fa8011b
Brian Bud <hello@brianbud.com>
2023-11-17 11:49:38 -0500

    about us content

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+export default function aboutUs() {
+  const content = document.querySelector("#content");
+
+  content.innerHTML = `
+    <div>
+        <p>
+        The Original Beef of Chicagoland, simply known as The Beef, was the family business of the Berzattos. 
+        It was originally run by Mikey Berzatto, but after his passing was passed down to his younger brother Carmen “Carmy” Berzatto.
+        </p>
+        <p>Carmen returned to Chicago from New York, where he had become a renowned chef, to run the restaurant with his sister Natalie and his brother’s best friend Richie. 
+        Together, they revamped the menu, hired a talented staff, and transformed the restaurant into a modern and elegant place, while still honoring Michael’s vision and values.</p>
+        <p>
+        "The Bear" a nickname given Michael to Carmy Berzatto, is now a modern fine dining experience with Chicago's talented chefs.
+        </p>
+
+    </div>
+    `;
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -17,6 +17,13 @@ export default function home() {
     <div>
         <h2>A Taste of Chicago and Beyond.</h2>
     </div>
+    <div>
+      <p>Welcome to The Bear, Chicago’s newest fine dining destination.</p> 
+      <p>We offer a seasonal tasting menu that showcases the best of local and global ingredients, prepared with creativity and care.</p>
+      <p>Our dishes are inspired by our personal stories, memories, and emotions, and we invite you to share them with us.</p> 
+      <p>Whether you’re looking for a romantic date night, a special occasion, or a culinary adventure, we’ve got you covered.</p> 
+      <p>Come and experience the magic of The Bear, where we make food with art and passion.</p>
+    </div>
   </section>
   
   `;
