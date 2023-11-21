fb5d9acb7626c5f9cb95f4092bbac3d5a7267502
Brian Bud <hello@brianbud.com>
2023-11-20 20:26:27 -0500

    add chef img and about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,12 @@
+import img from "chef.jpg";
+
 export default function aboutUs() {
   const about = document.createElement("div");
-
+  const image = document.createElement("img");
+  image.src = "./chef.jpg";
   about.innerHTML = `
     <div id="about">
+     <div>
         <p>
         The Original Beef of Chicagoland, simply known as The Beef, was the family business of the Berzattos. 
         It was originally run by Mikey Berzatto, but after his passing was passed down to his younger brother Carmen “Carmy” Berzatto.
@@ -12,7 +16,13 @@ export default function aboutUs() {
         <p>
         "The Bear" a nickname given Michael to Carmy Berzatto, is now a modern fine dining experience with Chicago's talented chefs.
         </p>
-
+      </div>
+      <div>
+        <img src="./chef.jpg">
+        <p>Hi, I’m Carmy, the executive chef and co-owner of The Beef, a sandwich shop in Chicago that serves delicious and creative food.</p> 
+        <p>I have a passion for cooking and a vision for transforming the traditional sandwich into a culinary masterpiece. </p>
+        I have worked in some of the finest restaurants in New York City, and I have learned from some of the best chefs in the world. I returned to my hometown to take over my family’s legacy and revitalize the restaurant with my innovative and creative vision. I lead my team with a mix of tough love and mentorship, and I strive to create the best food possible for my customers. I’m also a proud brother, a loyal friend, and a survivor of trauma. I’m always looking for new challenges and opportunities to grow as a chef and as a person. I hope you enjoy my food and my story. Welcome to The Bear!</p>
+      </div>
     </div>
     `;
 
