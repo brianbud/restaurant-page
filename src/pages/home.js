84bc5b66086a1249cebb57a426bbc8bf65366010
Brian Bud <hello@brianbud.com>
2023-11-21 20:43:06 -0500

    font style

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,8 +4,8 @@ export default function home() {
   hero.classList.add("home-container");
   hero.innerHTML = `
   <section id="home">
-      <h1>A Taste of Chicago and Beyond.</h1>
-      <p>Welcome to The Bear, Chicago’s newest fine dining destination.</p> 
+      <p>A Taste of Chicago and Beyond.</p>
+      <p>Chicago’s newest fine dining destination.</p> 
       <p>We offer a seasonal tasting menu that showcases the best of local and global ingredients, prepared with creativity and care.</p>
       <p>Our dishes are inspired by our personal stories, memories, and emotions, and we invite you to share them with us.</p> 
       <p>Whether you’re looking for a romantic date night, a special occasion, or a culinary adventure, we’ve got you covered.</p> 
--- src/pages/nav.js
+++ src/pages/nav.js
@@ -2,7 +2,7 @@ export default function navbar() {
   const content = document.querySelector("#content");
   const header = document.createElement("header");
   header.innerHTML = `
-    <div><h1>The Bear</h1></div>
+    <div><h1 id="title">The Bear</h1></div>
     <nav>
       <button>Home</button>
       <button>Menu</button>
